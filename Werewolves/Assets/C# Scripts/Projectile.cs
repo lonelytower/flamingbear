8a4a0a5100f7d719779d21d8291be9ce628db7bc
Kitsune <lachlangodthelp@hotmail.com>
2014-01-08 12:32:23 +1100

    4 basic scripts are in, as well as a projectile base which will be used for all the projectiles in the game. You can either make your own scene and attach the scripts to a new cube or you can just modify the existing one.

---
+++ Werewolves/Assets/C# Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	
+	
+	//Inherited from weapon upon initialization
+	public float velocity;
+	public float lifetime;
+	public float damage; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(transform.rotation.eulerAngles.ToString());
+		this.transform.Translate(new Vector3(0,(transform.rotation.eulerAngles.z*velocity*Time.deltaTime),0));
+		lifetime=lifetime-Time.deltaTime;
+		if(lifetime<=0){
+			GameObject.DestroyImmediate(this.gameObject); //And play expire animation once we get it.
+		}
+	}
+}
---
+++ Werewolves/Assets/C# Scripts/WeaponSystem.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSystem : MonoBehaviour {
+	
+	//This is where we do all the weapon switching and stuff.
+	int weaponSlot = 1; //2 slots right now, set to 1 for melee and 2 for ranged
+	bool melee = true; //Whether or not the equipped weapon is melee, will be inherited from the weapon itself later
+	GameObject equippedWeapon; //The weapon you have equipped, selected from the Resources/weapons folder after we make them all
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Fire3")>0){ //Left shift and middle mouse button
+			//Switch weapons
+			switch(weaponSlot){
+			case(1):
+				weaponSlot=2;
+				melee = false;
+				break;
+			case(2):
+				weaponSlot=1;
+				melee = true;
+				break;
+			default:
+				break;
+			}
+		}
+		if(Input.GetAxis("Fire1")>0){ //Left Control and left mouse button, remap latter
+			if(melee){
+				//Swing, thrust, bash, etc
+			} else {
+				//Fire projectile! Projecticle sprite inherited from weapon later
+				GameObject projectileObj;
+				projectileObj = GameObject.Instantiate(Resources.Load("Weapons/ProjectileBase"),Vector3.forward/2,this.transform.rotation) as GameObject;
+				projectileObj.GetComponent<Projectile>().velocity = 1; //Set it to the weapon value.
+				projectileObj.transform.rotation = new Quaternion(0,0,this.transform.rotation.z);
+			}
+		}
+	}
+}
