c411ff785236da1c9ac5df98d3909d4f7943f856
KitsuneEnvy <lachlangodthelp@hotmail.com>
2014-02-02 22:46:05 +1100

    Added layer transparency, when you go behind objects with the foreground tag all the objects with that tag go transparent. Can be used for buildings or other stuff w/e

---
+++ Werewolves/Assets/C# Scripts/LayerHide.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerHide : MonoBehaviour {
+
+	bool hide = false;
+	RaycastHit rayInfo;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(Physics.Raycast(Camera.main.ScreenPointToRay(GameObject.FindGameObjectWithTag("Player").transform.position),out rayInfo)){
+//			Debug.Log("Hitting : " + rayInfo.collider.name);
+//			if(rayInfo.collider.gameObject.GetComponent<SpriteRenderer>().sortingLayerName == "Foreground"){
+//				hide = true;
+//			} else {
+//				hide = false;
+//			}
+//		}
+		if(hide){
+			foreach(GameObject item in GameObject.FindGameObjectsWithTag("Foreground")){
+				
+				item.renderer.material.color = new Color(1,1,1,0.4f);
+			}
+		} else {
+			foreach(GameObject item in GameObject.FindGameObjectsWithTag("Foreground")){
+				
+				item.renderer.material.color = new Color(1,1,1,1);
+			}
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Foreground"){
+			hide = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag == "Foreground"){
+			hide = false;
+		}
+	}
+}
