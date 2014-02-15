fb659d1d62bf168b1f91fa16e5ee5f27a78ec3d3
KitsuneEnvy <lachlangodthelp@hotmail.com>
2014-02-15 16:44:12 +1100

    Breakable objects added, needs sprites for them before more work can be done.

---
+++ Werewolves/Assets/Breakable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breakable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collider){
+		if(collider.gameObject.tag == "Projectile"){
+			//change to breaking animation
+			//After animation delete and spawn any items from drop table
+			Destroy(this.gameObject,0.2f);
+		}
+	}
+}
