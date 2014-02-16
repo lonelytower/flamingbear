06e724b6e63bf8ea756860d3d507ad71ce68b809
KitsuneEnvy <lachlangodthelp@hotmail.com>
2014-02-16 16:40:43 +1100

    Added in Heart Display for player health

---
+++ Werewolves/Assets/HealthDisplay.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthDisplay : MonoBehaviour {
+
+	public Sprite HeartMax;
+	public Sprite HeartSecond;
+	public Sprite HeartThird;
+	public Sprite HeartEmpty;
+
+	GameObject Heart1;
+	GameObject Heart2;
+	GameObject Heart3;
+
+	float playerHealth;
+
+	// Use this for initialization
+	void Start () {
+		Heart1 = GameObject.Find("Heart1");
+		Heart2 = GameObject.Find("Heart2");
+		Heart3 = GameObject.Find("Heart3");
+	}
+	// Update is called once per frame
+	void Update () {
+		playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Stats>().health;
+		if(playerHealth>88){
+			Heart1.GetComponent<SpriteRenderer>().sprite = HeartMax;
+		} else if(playerHealth>77){
+			Heart1.GetComponent<SpriteRenderer>().sprite = HeartSecond;
+		} else if(playerHealth>66){
+			Heart1.GetComponent<SpriteRenderer>().sprite = HeartThird;
+		} else if(playerHealth>55){
+			Heart1.GetComponent<SpriteRenderer>().sprite = HeartEmpty;
+			Heart2.GetComponent<SpriteRenderer>().sprite = HeartMax;
+		} else if(playerHealth>44){
+			Heart2.GetComponent<SpriteRenderer>().sprite = HeartSecond;
+		} else if(playerHealth>33){
+			Heart2.GetComponent<SpriteRenderer>().sprite = HeartThird;
+		} else if(playerHealth>22){
+			Heart2.GetComponent<SpriteRenderer>().sprite = HeartEmpty;
+			Heart3.GetComponent<SpriteRenderer>().sprite = HeartMax;
+		} else if(playerHealth>11){
+			Heart3.GetComponent<SpriteRenderer>().sprite = HeartSecond;
+		} else if(playerHealth>0){
+			Heart3.GetComponent<SpriteRenderer>().sprite = HeartThird;
+		} else if(playerHealth<=0||playerHealth==null){
+			Heart3.GetComponent<SpriteRenderer>().sprite = HeartEmpty;
+		}
+	}
+}
