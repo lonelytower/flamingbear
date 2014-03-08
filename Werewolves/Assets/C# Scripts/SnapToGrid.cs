f3a933e185a07d1792fde99b598b1787597c9fb6
KitsuneEnvy <lachlangodthelp@hotmail.com>
2014-03-08 13:43:17 +1100

    Added grid snapping, can be applied to anything but mostly ground tiles.

---
+++ Werewolves/Assets/C# Scripts/SnapToGrid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SnapToGrid : MonoBehaviour {
+
+	float snapDistance = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float snapInverse = 1/snapDistance;
+		
+		float x, y, z;
+		
+		// if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
+		// so 1.45 to nearest .5 is 1.5
+		x = Mathf.Round(transform.position.x * snapInverse)/snapInverse;
+		y = Mathf.Round(transform.position.y * snapInverse)/snapInverse;
+		this.transform.position = new Vector3(x,y,0);
+	}
+}
