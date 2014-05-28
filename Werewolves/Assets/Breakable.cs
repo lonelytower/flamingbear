db61c5a9d036fb3b0acf93c550ae602c9bf0757d
KitsuneEnvy <lachlangodthelp@hotmail.com>
2014-05-28 13:25:58 +1000

    Added items dropping from crates. Only items for now.

--- Werewolves/Assets/Breakable.cs
+++ Werewolves/Assets/Breakable.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class Breakable : MonoBehaviour {
 
-
+	bool broken = false;
 	public Sprite initial;
-	public Sprite broken;
+	public Sprite brokenSprite;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,15 +13,24 @@ public class Breakable : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(broken){
+			this.GetComponent<SpriteRenderer>().sprite = brokenSprite;
+			this.GetComponent<BoxCollider2D>().enabled = false;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collider){
 		if(collider.gameObject.tag == "Projectile"){
-			this.GetComponent<SpriteRenderer>().sprite = broken;
+			broken = true;
+			if(this.GetComponent<ItemDrops>()!=false){
+				this.GetComponent<ItemDrops>().triggerDrop();
+			}
 			//change to breaking animation
 			//After animation delete and spawn any items from drop table
 			//Destroy(this.gameObject,0.2f);
 		}
+	}
+	public bool returnBroken(){
+		return broken;
 	}
 }
---
+++ Werewolves/Assets/ItemDrops.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ItemDrops : MonoBehaviour {
+	//knife 25 sword 20 trap 30 wolfsbane 40 revolver 10 shield 15 Mace 5 Musket 5 
+	List<string> dropTable = new List<string>();
+	List<float> dropTablePct = new List<float>();
+	Breakable breakingScript;
+	public int id = 0; //0 = item crate, 1 = weapon crate, 2 = chest, 3=dead bodies(npc), 4 = dead bodies(player)
+
+	// Use this for initialization
+	void Start () {
+		breakingScript = this.GetComponent<Breakable>();
+		dropTable.Add("Nothing");
+		switch(id){
+		case(1):
+			dropTable.Add("Trap");
+			dropTable.Add("Wolfsbane");
+			dropTablePct.Add(30);
+			dropTablePct.Add(30);
+			dropTablePct.Add(40);
+			break;
+		case(2):
+			dropTable.Add("Knife");
+			dropTable.Add("Sword");
+			dropTable.Add("Revolver");
+			dropTable.Add("Shield");
+			dropTable.Add("Mace");
+			dropTable.Add("Musket");
+			dropTablePct.Add(20);
+			dropTablePct.Add(25);
+			dropTablePct.Add(20);
+			dropTablePct.Add(10);
+			dropTablePct.Add(15);
+			dropTablePct.Add(5);
+			dropTablePct.Add(5);
+			break;
+		case(3):
+			break;
+		default:
+			break;
+		}
+
+	
+	}
+
+	public void triggerDrop(){
+		float value = Random.Range(0,100);
+		Debug.Log(value.ToString());
+		switch(id){
+		case(0):
+			if(value>=30&&value<=60){
+				Debug.Log("Fire");
+				GameObject.Instantiate(Resources.Load("Items/Trap"),this.transform.position,this.transform.rotation);
+			}
+			if(value>60){
+				GameObject.Instantiate(Resources.Load("Items/Wolfsbane"),this.transform.position,this.transform.rotation);
+			}
+			break;
+		default:
+			Debug.Log("Default");
+			break;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
